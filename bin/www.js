a253294c20d5145e6c312d41f7c13e21d707d5de
Amin <amin@sudofy.com>
2017-12-21 18:38:35 +0500

    Add main routes to www.js

--- bin/www.js
+++ bin/www.js
@@ -3,12 +3,14 @@
 /**
  * Module dependencies.
  */
-
+require('module-alias/register');
 var app = require('../main');
 var debug = require('debug')('api:server');
 var http = require('http');
-var log = require('tracer').console({format : "{{message}}  - {{file}}:{{line}}"}).log;
+var log = require('log');
 var art = require('ascii-art');
+var serverMessages = require('../server/messages');
+var serverCodes = require('../server/codes');
 /**
  * Get port from environment and store in Express.
  */
@@ -29,10 +31,52 @@ var server = http.createServer(app);
 server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening);
-
+//Requiring routes
+require('../routes/router')(app);
 /**
  * Normalize a port into a number, string, or false.
  */
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next({
+    status: 404,
+    message: serverMessages.server.ROUTE_NOT_FOUND,
+    data: null
+  });
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function (err, req, res, next) {
+    log('##API--ERR');
+    log(err);
+    res.status(err.status || serverCodes.SERVER_ERROR);
+    res.json({
+      message: err.message,
+      data: err.data,
+      success: false
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+  log('##API--ERR');
+  log(err);
+  res.status(err.status || serverCodes.SERVER_ERROR);
+  res.json({
+    message: err.message,
+    data: null,
+    success: false
+  });
+});
+process.on('uncaughtException', function (err) {
+  log('Caught exception: ' + err);
+});
 
 function normalizePort(val) {
   var port = parseInt(val, 10);
@@ -89,10 +133,10 @@ function onListening() {
     : 'port ' + addr.port;
   debug('Listening on ' + bind);
 
-  art.font('base-codeapi    server', 'Doom', function(rendered){
+  art.font('base-codeapi    server', 'Doom', function (rendered) {
     console.log(rendered);
   });
 
-  log("Server Running on port "+ app.get('port'));
+  log("Server Running on port " + app.get('port'));
   log("###########################################################################");
 }
