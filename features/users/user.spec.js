f07ea6b2fd68bd4af16b729207e22e4eb242e5ee
daniyal <daniyal@sudofy.com>
2018-03-02 11:25:37 +0500

    Solved user spec issues

--- features/users/user.spec.js
+++ features/users/user.spec.js
@@ -1,27 +1,29 @@
-require('module-alias/register');
 const config = require('../../spec/config');
 const frisby = require('frisby');
-const Joi = frisby.Joi;
+let token;
 
 describe(`User register :`, () => {
-  it(`should save information of user`, () => {
+  it(`should save information of user`, done => {
+
     frisby
       .post(`${config.domain}/user/register`, {
+
         username: `dany600`,
         password: `12345`,
         firstname: `faizan`,
         lastname: `saeed`,
       })
       .expect(`status`, 200)
-      .expect(`json`, `errors`, {
-        message: `User registered Successfully`,
+      .expect(`json`, {
+        message: `User registered Successfully`
       })
-      .done();
+      .done(done);
+
   });
 
 });
 describe(`User Login :`, () => {
-  it(`it should login the user`, () => {
+  it(`it should login the user`, done => {
     frisby
       .post(`${config.domain}/user/login`, {
         username: `dany600`,
@@ -29,68 +31,65 @@ describe(`User Login :`, () => {
       })
       .expect(`status`, 200)
       .expect(`json`, {
-
         message: `Login successful!`,
-        success: true,
-        data: {
-          token: Joi.string(),
-          user: {
-            firstname: Joi.string(),
-            lastname: Joi.string(),
-            __v: 0,
-            username: `dany600`,
-            _id: Joi.string()
-          }
-        }
       })
-
-      .done();
-
+      .then(res => {
+        token = res._body.data.token;
+      })
+      .done(done);
   });
 
-  it(`it should  fail to login the user with username missing`, () => {
+  it(`it should  fail to login the user with username missing`, done => {
     frisby
       .post(`${config.domain}/user/login`, {
         password: `12345`
       })
       .expect(`status`, 500)
-      .done();
+      .done(done);
 
   });
 
-  it(`it should  fail to login the user with password missing`, () => {
+  it(`it should  fail to login the user with password missing`, done => {
     frisby
       .post(`${config.domain}/user/login`, {
+
         username: `dany600`
       })
       .expect(`status`, 500)
-      .done();
+      .done(done);
 
   });
 });
 describe(`User Logout :`, () => {
-  it(`it should logout the user`, () => {
+  it(`it should logout the user`, done => {
     frisby
       .get(`${config.domain}/user/logout`, {
-
+        headers: {
+          'x-access-token': token,
+          origin: config.domain,
+          'Content-Type': 'application/json',
+        }
       })
       .expect(`status`, 200)
-      .done();
+      .done(done);
 
   });
 });
 describe(`Get all users :`, () => {
-  it(`it should fail to get all users`, () => {
+  it(`it should fail to get all users`, done => {
     frisby
       .get(`${config.domain}/user/`, {
-
+        headers: {
+          'x-access-token': token,
+          origin: config.domain,
+          'Content-Type': 'application/json',
+        }
       })
       .expect(`status`, 403)
       .expect(`json`, {
-        message: `No token provided!`,
-        success: false
+        message: `You are not authorized to perform this operation!`
       })
-      .done();
+      .done(done);
 
   });
 });
